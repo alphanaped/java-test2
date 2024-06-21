5a47e787a35b7863322e4e54ce98ef0c54c57e5c
alphanaped <160116198+alphanaped@users.noreply.github.com>
2024-06-21 11:06:52 +0000

    edc statments

---
+++ 0001-expression.java
@@ -0,0 +1,15 @@
+class Expression{
+    public static void main(String[] arg){
+        System.out.println("Expression in Java");
+
+        int cadense = 0;
+        aValue = 2323;
+        int numbers[] = new int[4];
+        numbers[0] = 3;
+        numbers[2] = 5;
+        for(int element:numbers) {
+            System.out.println(element);
+        }
+        System.out.println(aValue);
+    }
+}
